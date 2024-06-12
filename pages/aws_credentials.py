2d64ec946a1182f5c613570f5d61dff889361f71
Erick Edwin Hernandez Macias VS CODE <gomin_17@gotmail.com>
2024-06-12 13:16:57 -0600

    chore: Add initial files for Python development environment and pages

---
+++ pages/aws_credentials.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import boto3
+
+# Este formulario de streamlit permite configurar las credenciales de AWS
+# para poder acceder a los servicios de AWS.
+
+st.markdown("# Page1")
+
+
---
+++ pages/rerun_spei.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import boto3
+# Este formulario de streamlit permitita mediante la eleccion de credenciales de aws
+# poder hacer un re ejecucion de una state machine en aws
+
+st.sidebar.markdown("## Descripcion")
